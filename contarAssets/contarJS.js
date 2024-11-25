d8dd9cabfc2e4382820386bb6efa91653cc26cf9
João Flávio <joaoflaviog1@gmail.com>
2024-11-25 14:53:25 -0300

    v1 (working)

--- contarAssets/contarJS.js
+++ contarAssets/contarJS.js
@@ -1,48 +1,89 @@
 function rodar() {
-    let pes = document.querySelectorAll("p")
-    let p = []
-    let contador = {}
+
+    class Contagem {
+        constructor(num,atalho = "x",valor = 0){
+            this.num = num
+            this.atalho = atalho
+            this.valor = valor
+        }
+    }
+
+    let p = document.querySelector("p")
     let divs = 1
+    let valores = [new Contagem(0,"ArrowUp")]
+    let contadores = document.querySelectorAll(".contador")
     let fileira = document.querySelectorAll(".fileira")[0]
+    let botoes = document.querySelectorAll("button")
+    let zero = 0
     const section = document.querySelector("section")
-    document.addEventListener('keyup', function (e) {
-        if (e.key === "ArrowUp") aumenta()
+    
+    document.addEventListener('keyup', (e) => {
+        for (valor of valores){
+            if (e.key == valor.atalho){
+                aumenta(zero)
+            }
+            zero++
+        }
+        zero = 0
         if (e.key === "ArrowRight") add()
     })
-    function aumenta(num) {
-        contador.num = "true"
-        console.log(contador)
+    section.addEventListener("click", (e) => {
+        for (botao of botoes){
+            if (e.target == botao){
+                setAtalho(zero)
+            }
+            zero++
+        }
+        zero = 0
+        // for (contador of contadores){
+        //     if(e.target == contador || e.target == contador.querySelector("p")){ 
+        //         alvo = contador
+        //     }
+        // } 
+    })
+
+    function aumenta(alvo) {
+        valores[alvo].valor += 1
+        contadores[alvo].querySelector("p").innerText =  valores[alvo].valor
     }
-    function add(){
-        if (divs % 5 == 0){
+    function add() {
+        if (divs % 5 == 0) {
             criar("fileira")
         }
         criar("div")
-        for(pNovo in pes){
-            p.push(pNovo)
-        }
         divs++
+        contadores = document.querySelectorAll(".contador")
+        botoes = document.querySelectorAll("button")
     }
-    function criar(elemento){
+    function criar(elemento) {
         let novaFila = document.createElement("div")
         let novoContador = document.createElement("div")
         let novoP = document.createElement("p")
         let novoBotao = document.createElement("button")
         novaFila.classList.add("fileira")
-        if (elemento === "div"){
-            novoContador.classList.add("contador",`c${divs}`)
-            novoBotao.classList.add("unset")
+        
+        if (elemento === "div") {
+            novoContador.classList.add("contador", `c${divs}`)
+            novoBotao.classList.add("unset", `b${divs}`)
             novoBotao.innerText = "Adicione um atalho"
             novoP.innerText = "0"
-            novoContador.append(novoP,novoBotao)
+            novoContador.append(novoP, novoBotao)
             fileira.appendChild(novoContador)
+            valores.push(new Contagem(divs))
+            console.log(valores)
         }
-        if(elemento === "fileira"){
+        if (elemento === "fileira") {
             section.appendChild(novaFila)
             fileira = document.querySelectorAll(".fileira")[divs / 5]
         }
     }
+    function setAtalho(alvo) {
+        //botoes[alvo].innerText = "|"
+        const atalhoEscrito = prompt("Insira o atalho desejado").toLowerCase()[0]
+        valores[alvo].atalho = atalhoEscrito
+        botoes[alvo].innerText = valores[alvo].atalho.toUpperCase()
+    }
 }
 
 // terminar de setar os valores de cada div usando o let divs e o objeto contador e colocar os atalhos,
-// {c0: {contador:2, atalho:"y"}, c1: {contador:0, atalho:"t"}} 
+// colocar uma função para parar um contador
